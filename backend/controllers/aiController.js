9d1432aadf866f92019239af5c07b2b1fff8fc01
Devendra4772 <devendrakumaryadav532@gmail.com>
2026-05-19 02:03:46 +0530

    Updated AI Controller

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -8,19 +8,23 @@ exports.getRecommendation = async (req, res) => {
 
     const prompt = `
     Employee Name: ${employee.name}
+    Department: ${employee.department}
     Performance Score: ${employee.performanceScore}
     Skills: ${employee.skills}
+    Experience: ${employee.experience}
 
     Give:
     1. Promotion Recommendation
     2. Training Suggestions
-    3. Feedback
+    3. Employee Feedback
     `;
 
     const response = await axios.post(
+
       "https://openrouter.ai/api/v1/chat/completions",
+
       {
-        model: "openai/gpt-3.5-turbo",
+        model: "mistralai/mistral-7b-instruct:free",
 
         messages: [
           {
@@ -32,19 +36,35 @@ exports.getRecommendation = async (req, res) => {
 
       {
         headers: {
+
           Authorization:
-          `Bearer ${process.env.OPENROUTER_API_KEY}`
+          `Bearer ${process.env.OPENROUTER_API_KEY}`,
+
+          "Content-Type": "application/json",
+
+          "HTTP-Referer":
+          "https://emp-manage-1-rtxx.onrender.com",
+
+          "X-Title":
+          "Emp Manage"
         }
       }
     );
 
-    res.json(response.data);
+    res.json({
+      recommendation:
+      response.data.choices[0].message.content
+    });
 
   } catch (error) {
 
-    res.status(500).json({
-      message: error.message
-    });
+    console.log(
+      error.response?.data || error.message
+    );
 
+    res.status(500).json({
+      message:
+      error.response?.data || error.message
+    });
   }
 };
