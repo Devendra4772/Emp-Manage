3e7e9a4fce0b89b37e67aa7bca1eb61160ab6611
Devendra4772 <devendrakumaryadav532@gmail.com>
2026-05-19 01:33:18 +0530

    Frontend setup completed

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://emp-manage-0w89.onrender.com"
+  baseURL: "https://emp-manage-0w89.onrender.com/api"
 });
 
 export default api;
